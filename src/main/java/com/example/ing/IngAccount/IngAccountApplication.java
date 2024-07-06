4c45bcc155d384ff78315d999f557c1c96d4cf8c
rubenhaal <73060603+rubenhaal@users.noreply.github.com>
2024-07-06 08:39:43 +0200

    Initial commit
    First Commit

---
+++ src/main/java/com/example/ing/IngAccount/IngAccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.ing.IngAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ing/IngAccount/IngAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ing.IngAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
